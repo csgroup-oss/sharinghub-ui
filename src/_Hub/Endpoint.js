07e61bfbff524112855bc9ed4208c181c25b38b3
Clément GUICHARD <clement.guichard@csgroup.eu>
2024-11-12 14:12:15 +0100

    fix: use BASE_URL for login redirect_uri instead of simple origin

--- src/_Hub/Endpoint.js
+++ src/_Hub/Endpoint.js
@@ -8,7 +8,7 @@ export const DOCS_URL  =  BASE_URL.concat('docs/');
 
 export const STAC_ROOT_URL = API_URL.concat('stac/');
 export const STAC_SEARCH = API_URL.concat('stac/search');
-export const LOGIN_URL = API_URL.concat('auth/login?redirect_uri='.concat(window.location.origin));
+export const LOGIN_URL = API_URL.concat('auth/login?redirect_uri='.concat(BASE_URL));
 export const LOGOUT_URL = API_URL.concat('auth/logout');
 export const USER_INFO = API_URL.concat('auth/info');
 export const PROXY_URL = API_URL.concat('proxy/');
